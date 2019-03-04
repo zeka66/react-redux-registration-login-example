4d14048c4e61f42d12a2c9aab25290d6a7896795
Zeka66 <zeka66@gmail.com>
2019-03-04 20:11:10 +0200

    deploy file

--- deploy.sh
+++ deploy.sh
@@ -6,14 +6,17 @@ DOCKER_USERNAME=$2
 DOCKER_PASSWORD=$3
 
 # Create publish artifact
-dotnet publish -c Release .
-cp Dockerfile bin/Release/netcoreapp2.2/publish/
+
+# dotnet publish -c Release .
+
+# cp Dockerfile bin/Release/netcoreapp2.2/publish/
 
 # Build the Docker images
-docker build -t zeka66/aspnet-core-registration-login-api:$TAG bin/Release/netcoreapp2.2/publish/.
-docker tag zeka66/aspnet-core-registration-login-api:$TAG zeka66/aspnet-core-registration-login-api:latest
+docker build -t zeka66/react-redux-registration-login-example:$TAG bin/Release/netcoreapp2.2/publish/.
+docker tag zeka66/react-redux-registration-login-example:$TAG zeka66/react-redux-registration-login-example:latest
 
 # Login to Docker Hub and upload images
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker push zeka66/aspnet-core-registration-login-api:$TAG
-docker push zeka66/aspnet-core-registration-login-api:latest
+docker push zeka66/react-redux-registration-login-example:$TAG
+docker push zeka66/react-redux-registration-login-example:latest
+
