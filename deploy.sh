303bc3b7ae90217b158204e1e342135ebe64b75a
zeka66 <zeka66@gmail.com>
2019-03-05 10:15:04 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,11 @@ DOCKER_PASSWORD=$3
 
 
 # Build the Docker images
-docker build -t zeka66/react-redux-registration-login-example .
+docker build -t zeka66/aspnet-core-registration-login-api:$TAG 
+docker tag zeka66/react-redux-registration-login-example:$TAG zeka66/react-redux-registration-login-example:latest
 
 # Login to Docker Hub and upload images
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker push zeka66/react-redux-registration-login-example:$TAG
-
+docker push zeka66/aspnet-core-registration-login-api:latest
 
