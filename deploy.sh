1d46ae0a45eb8053bdc55bcaf37b14b462f5074f
zeka66 <zeka66@gmail.com>
2019-03-05 10:36:06 +0200

    delply

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,8 @@ DOCKER_PASSWORD=$3
 
 
 # Build the Docker images
-docker build -t zeka66/react-redux-registration-login-example:$TAG react-redux-registration-login-example .
-docker tag zeka66/react-redux-registration-login-example:$TAG zeka66/react-redux-registration-login-example:latest
+docker build -t zeka66/react-redux-registration-login-example .
+
 
 # Login to Docker Hub and upload images
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
